efe23e5e8bc162ed1407148577db5659b67020b5
EC2 Default User <92033602+shot-a07@users.noreply.github.com>
2021-12-07 08:54:40 +0000

    Add #5

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,32 +1,32 @@
 class BooksController < ApplicationController
   before_action :authenticate_user!
   before_action :ensure_correct_user, only: [:edit, :update, :destroy]
-  
+
   def new
     @book = Book.new
-    
+
   end
-  
+
   def show
     @book = Book.find(params[:id])
     @new_book = Book.new
     @book_comment = BookComment.new
-    
-    
+
+
   end
 
   def index
     @books = Book.all
     @user = current_user
     @book = Book.new
-    
+
   end
 
   def create
     @book = Book.new(book_params)
-    
+
     @book.user_id = current_user.id
-    
+
     if @book.save
       flash[:noting] = "You have created book successfully."
       redirect_to book_path(@book.id)
@@ -52,22 +52,22 @@ class BooksController < ApplicationController
       redirect_to book_path(@book), notice: "You have updated book successfully."
     else
       render :edit
-      
+
     end
   end
-  
+
 
   def destroy
     @book = Book.find(params[:id])
     @book.destroy
     redirect_to books_path, notice: "successfully delete book"
-    
+
   end
 
   private
   def book_params
     params.require(:book).permit(:title, :body)
-    
+
   end
   def ensure_correct_user
       @book = Book.find(params[:id])
@@ -75,7 +75,7 @@ class BooksController < ApplicationController
         redirect_to books_path
       end
   end
-  
+
 
 
 
