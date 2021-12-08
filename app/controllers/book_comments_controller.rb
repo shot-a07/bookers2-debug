323aeb475f1d3f37e74e52af4ffa9c1227e3127e
EC2 Default User <92033602+shot-a07@users.noreply.github.com>
2021-12-08 16:28:51 +0000

    Update #6

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -3,11 +3,11 @@ class BookCommentsController < ApplicationController
   
   def create
     @book = Book.find(params[:book_id])
-    @comment = BookComment.new(book_comment_params)
+    comment = BookComment.new(book_comment_params)
     comment.user_id = current_user.id
     comment.book_id = @book.id
     if comment.save
-    redirect_to book_path(@book.id)
+    redirect_to book_path(@book.id), notice: 'コメントを投稿しました'
     else
       @new_book = Book.new
       @book_comment = BookComment.new
@@ -22,7 +22,7 @@ class BookCommentsController < ApplicationController
     @book = Book.find(params[:book_id])
     book_comment = @book.book_comments.find(params[:id])
     book_comment.destroy
-    redirect_to book_path(params[:book_id])
+    redirect_to book_path(params[:book_id]), alert: 'コメントを削除しました'
   end
   
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :books
+  has_many :books,dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
